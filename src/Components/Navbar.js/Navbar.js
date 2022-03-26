c8e082583f089ac21dec5c43858289bcfdc1a6e7
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-26 11:13:39 +0530

    navbar

--- src/Components/Navbar.js/Navbar.js
+++ src/Components/Navbar.js/Navbar.js
@@ -1,44 +1,78 @@
-import React from "react";
-import memories from "../../images/memories.png";
-import { AppBar, Typography, Toolbar, Button, Avatar } from "@material-ui/core";
-import useStyles from "./styles.js";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
+
+import memoriesLogo from "../../images/memoriesLogo.png";
+import memoriesText from "../../images/memoriesText.png";
+import * as actionType from "../../constants/actionTypes";
+import useStyles from "./styles";
+
 const Navbar = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
   const classes = useStyles();
-  const user = null;
+
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push("/auth");
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <div className={classes.brandContainer}>
-        <Typography
+      <Link to="/" className={classes.brandContainer}>
+        <img
           component={Link}
           to="/"
-          className={classes.heading}
-          variant="h2"
-          align="center"
-        >
-          Hearty Memories
-        </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </div>
+          src={memoriesText}
+          alt="icon"
+          height="45px"
+        />
+        <img
+          className={classes.image}
+          src={memoriesLogo}
+          alt="icon"
+          height="40px"
+        />
+      </Link>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
               className={classes.purple}
-              alt={user.result.name}
-              src={user.result.imageUrl}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
             >
-              {user.result.name.charAt(0)}
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
-              {user.result.name}
+              {user?.result.name}
             </Typography>
             <Button
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
-              Log Out
+              Logout
             </Button>
           </div>
         ) : (

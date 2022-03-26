9ec834a508c5b496b1b0cc75f9b2b2fd437b7ad2
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-26 11:04:28 +0530

    posts

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API = axios.create({ baseURL: "http://localhost:7000" });
+const API = axios.create({ baseURL: 'http://localhost:7000' });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile")).token
-    }`;
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
   }
 
   return req;
@@ -21,5 +19,6 @@ export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-export const signIn = (formData) => API.post("/user/signin", formData);
-export const signUp = (formData) => API.post("/user/signup", formData);
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);

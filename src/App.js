e952506b2cfc5fefaebe4d3d95f924e7a9faafc4
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-26 11:32:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-
-import Navbar from "./Components/Navbar.js/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import Auth from "./Components/Auth/Auth"; 
+import Auth from "./Components/Auth/Auth";
+import PostDetails from "./Components/PostDetails/PostDetails";
+import Navbar from "./Components/Navbar/Navbar";
+import CreatorOrTag from "./components/CreatorOrTag/CreatorOrTag";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
 
   return (
-  <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/auth"element={<Auth/>}/>
-      </Routes>
-      
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={() => <Navigate to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route
+            path={["/creators/:name", "/tags/:name"]}
+            element={<CreatorOrTag />}
+          />
+          <Route
+            path="/auth"
+            element={() => (!user ? <Auth /> : <Navigate to="/posts" />)}
+          />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 };

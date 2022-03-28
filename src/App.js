1bb0d907a0c855a3e7a34cbeb8d9fba87f3f6372
Sangeetha Kumarasamy <89238309+SangeethaKumarasamy@users.noreply.github.com>
2022-03-28 06:22:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate} from "react-router-dom";
+
 import Home from "./Components/Home/Home";
 import Auth from "./Components/Auth/Auth"; 
 import PostDetails from './Components/PostDetails/PostDetails';
 import Navbar from './Components/Navbar/Navbar';
 import CreatorOrTag from "./Components/CreatorOrTag/CreatorOrTag"
+
+
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
-  <BrowserRouter>
+  <Router>
     <Container maxWidth="lg">
       <Navbar />
       <Routes>
-      <Route path="/" exact element={() => <Navigate to="/posts" />} />
+      <Route path="/" exact element={() => <Navigate replace to="/posts" />} />
         <Route path="/posts" element={<Home/>}/>
         <Route path="/posts/search" element={<Home/>}/>
         <Route path="/posts/:id" element={<PostDetails/>}/>
         <Route path={['/creators/:name', '/tags/:name']} element={<CreatorOrTag/>} />
-        <Route path="/auth" element={() => (!user ? <Auth /> : <Navigate to="/posts" />)} />
+        <Route path="/auth" element={() => (!user ? <Auth /> : <Navigate replace to="/posts" />)} />
       </Routes>
       
     </Container>
-    </BrowserRouter>
+    </Router>
   );
 };
 
